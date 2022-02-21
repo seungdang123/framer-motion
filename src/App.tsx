893b6d248b7fb393e2fe337f9083f0cc29e08914
seungdang123 <seungdang123@gmail.com>
2022-02-22 00:54:06 +0900

    Drag and Drop motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
@@ -18,15 +19,39 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* overflow: hidden; */
+`;
+
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(45, 62 106)" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          // { top: -200, bottom: 200, left: -200, right: 200 }
+          dragConstraints={biggerBoxRef}
+          dragElastic={0.5}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
