abb10b8121b99b9622d0119678a1f8eafb62f520
seungdang123 <seungdang123@gmail.com>
2022-02-26 10:33:47 +0900

    SVG animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,59 @@
-import { useEffect, useRef } from "react";
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    61deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(64, 64, 64, 1) 35%,
+    rgba(208, 208, 208, 1) 100%
+  );
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  display: grid;
-  background-color: white;
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+  width: 300px;
+  height: 100px;
+  path {
+    stroke: white;
+    stroke-width: 5;
+  }
 `;
+
+const svg = {
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+  end: {
+    fill: "rgba(255, 255, 255, 1)",
+    pathLength: 1,
+  },
+};
 
 function App() {
-  // useMotionValue isn't related in state of React
-  // if x is changed, components won't be re-rendered
-  const { scrollYProgress } = useViewportScroll();
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 190, 238), rgb(163, 0, 238)",
-      "linear-gradient(135deg, rgb(0, 238, 151), rgb(231, 223, 68)",
-    ]
-  );
-
-  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 3]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box
-        style={{ x, rotateZ: rotateZ, scale: scale }}
-        drag="x"
-        dragSnapToOrigin
-      ></Box>
+    <Wrapper>
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <motion.path
+          variants={svg}
+          initial={"start"}
+          animate={"end"}
+          transition={{
+            default: {
+              duration: 2,
+            },
+            fill: { duration: 1, delay: 2 },
+          }}
+          d="M216.29 158.39H137C97 147.9 6.51 150.63 6.51 233.18c0 30.09 15 51.23 35 61-25.1 23-37 33.85-37 49.21 0 11 4.47 21.14 17.89 26.81C8.13 383.61 0 393.35 0 411.65c0 32.11 28.05 50.82 101.63 50.82 70.75 0 111.79-26.42 111.79-73.18 0-58.66-45.16-56.5-151.63-63l13.43-21.55c27.27 7.58 118.7 10 118.7-67.89 0-18.7-7.73-31.71-15-41.07l37.41-2.84zm-63.42 241.9c0 32.06-104.89 32.1-104.89 2.43 0-8.14 5.27-15 10.57-21.54 77.71 5.3 94.32 3.37 94.32 19.11zm-50.81-134.58c-52.8 0-50.46-71.16 1.2-71.16 49.54 0 50.82 71.16-1.2 71.16zm133.3 100.51v-32.1c26.75-3.66 27.24-2 27.24-11V203.61c0-8.5-2.05-7.38-27.24-16.26l4.47-32.92H324v168.71c0 6.51.4 7.32 6.51 8.14l20.73 2.84v32.1zm52.45-244.31c-23.17 0-36.59-13.43-36.59-36.61s13.42-35.77 36.59-35.77c23.58 0 37 12.62 37 35.77s-13.42 36.61-37 36.61zM512 350.46c-17.49 8.53-43.1 16.26-66.28 16.26-48.38 0-66.67-19.5-66.67-65.46V194.75c0-5.42 1.05-4.06-31.71-4.06V154.5c35.78-4.07 50-22 54.47-66.27h38.63c0 65.83-1.34 61.81 3.26 61.81H501v40.65h-60.56v97.15c0 6.92-4.92 51.41 60.57 26.84z"
+        ></motion.path>
+      </Svg>
     </Wrapper>
   );
 }
